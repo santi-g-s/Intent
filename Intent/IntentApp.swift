a5cc341b1edc3fce148d8e1ae61f1187134c437c
Santiago Garcia Santos <santiago@garciasantos.org>
2022-11-18 14:32:09 -0500

    Initial Commit

---
+++ Intent/IntentApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntentApp.swift
+//  Intent
+//
+//  Created by Santiago Garcia Santos on 18/11/2022.
+//
+
+import SwiftUI
+
+@main
+struct IntentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
