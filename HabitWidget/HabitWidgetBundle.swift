672c3c540355f04a74faa5b0e5cdc72fd1518334
Santiago Garcia Santos <santiago@garciasantos.org>
2023-12-21 12:32:54 -0600

    Set up files

---
+++ HabitWidget/HabitWidgetBundle.swift
@@ -0,0 +1,16 @@
+//
+//  HabitWidgetBundle.swift
+//  HabitWidget
+//
+//  Created by Santiago Garcia Santos on 21/12/2023.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct HabitWidgetBundle: WidgetBundle {
+    var body: some Widget {
+        HabitWidget()
+    }
+}
